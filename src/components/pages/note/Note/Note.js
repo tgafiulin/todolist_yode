a51ec3ef4b9cdc9b6d24fbaf2a2d629bdb7ea7a7
tgafiulin <t.gafiulin@ya.ru>
2021-02-14 10:55:48 +0300

    add dialogwindow for delete and cancel click

--- src/components/pages/note/Note/Note.js
+++ src/components/pages/note/Note/Note.js
@@ -1,33 +1,52 @@
 import './Note.scss'
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import TodoList from 'components/pages/note/TodoList/TodoList'
 import Button from "components/common/Button/Button";
+import DialogWindow from "components/common/DialogWindow/DialogWindow";
 import { useHistory } from "react-router-dom";
+import { deleteNote } from "app/reducers/notesReducer"
 
 function Note ({id}) {
     const [button, updateButton] = useState('');
+    const [openModal, toggleOpenModal] = useState(false);
     const notes = useSelector(state => state.notes.notes);
     const note = notes.filter((note) => id === note.id)[0];
     let history = useHistory();
+    let dispatch = useDispatch();
 
     const handleClick = (value) => {
-        updateButton(value); 
+        updateButton(value);
     }
 
-    const cancel = () => {
-        history.push("/");
+    const clickCancelOrDelete = (value) => {
+        updateButton(value);
+        toggleOpenModal(true);
+    }
+
+    const handleModalClick = (clickYes) => {
+        toggleOpenModal(false);
+        if (clickYes) {
+            if (button === 'delete') {
+                dispatch(deleteNote(id));
+            }
+            history.push("/");
+        }
     }
 
     return <div className="note">
-        <h1>Edit Note</h1>
+        <div className="note__title">
+            <h1>Edit Note</h1>
+            <Button onClick={() => clickCancelOrDelete('delete')} className="button button--red" value="Delete note"/>
+        </div>
         <TodoList note={note} button={button} />
         <div className="note__buttons">
             <Button onClick={() => handleClick('reset')} className="button button--blue" value="Reset"/>
             <Button onClick={() => handleClick('redo')} className="button button--black" value="Redo Change"/>
             <Button onClick={() => handleClick('save')} className="button" value="Save"/>
-            <Button onClick={cancel} className="button button--red" value="Cancel"/>
+            <Button onClick={() => clickCancelOrDelete('cancel')} className="button button--red" value="Cancel"/>
         </div>
+        {openModal ?  <DialogWindow accept={() => handleModalClick(true)} reject={() => handleModalClick(false)} /> : ''}
     </div>
 }
 
