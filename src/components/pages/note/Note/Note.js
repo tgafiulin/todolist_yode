20742386c06de18ed6000c907aa8829cef32f975
tgafiulin <t.gafiulin@ya.ru>
2021-02-15 02:30:09 +0300

    transfer buttons to todolist

--- src/components/pages/note/Note/Note.js
+++ src/components/pages/note/Note/Note.js
@@ -8,45 +8,25 @@ import { useHistory } from "react-router-dom";
 import { deleteNote } from "app/reducers/notesReducer"
 
 function Note ({id}) {
-    const [button, updateButton] = useState('');
     const [openModal, toggleOpenModal] = useState(false);
     const notes = useSelector(state => state.notes.notes);
     const note = notes.filter((note) => id === note.id)[0];
     let history = useHistory();
     let dispatch = useDispatch();
 
-    const handleClick = (value) => {
-        updateButton(value);
-    }
-
-    const clickCancelOrDelete = (value) => {
-        updateButton(value);
-        toggleOpenModal(true);
-    }
-
-    const handleModalClick = (clickYes) => {
+    const acceptModal = () => {
         toggleOpenModal(false);
-        if (clickYes) {
-            if (button === 'delete') {
-                dispatch(deleteNote(id));
-            }
-            history.push("/");
-        }
+        dispatch(deleteNote(id));
+        history.push("/");
     }
 
     return <div className="note">
         <div className="note__title">
             <h1>Edit Note</h1>
-            <Button onClick={() => clickCancelOrDelete('delete')} className="button button--red" value="Delete note"/>
+            <Button onClick={() => toggleOpenModal(true)} className="button button--red" value="Delete note"/>
         </div>
-        <TodoList note={note} button={button} />
-        <div className="note__buttons">
-            <Button onClick={() => handleClick('reset')} className="button button--blue" value="Reset"/>
-            <Button onClick={() => handleClick('redo')} className="button button--black" value="Redo Change"/>
-            <Button onClick={() => handleClick('save')} className="button" value="Save"/>
-            <Button onClick={() => clickCancelOrDelete('cancel')} className="button button--red" value="Cancel"/>
-        </div>
-        {openModal ?  <DialogWindow accept={() => handleModalClick(true)} reject={() => handleModalClick(false)} /> : ''}
+        <TodoList note={note} />
+        {openModal ?  <DialogWindow accept={() => acceptModal()} reject={() => toggleOpenModal(false)} /> : ''}
     </div>
 }
 
--- src/components/pages/note/TodoList/TodoList.js
+++ src/components/pages/note/TodoList/TodoList.js
@@ -1,13 +1,14 @@
 import './TodoList.scss'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Todo from 'components/pages/note/Todo/Todo'
 import DialogWindow from "components/common/DialogWindow/DialogWindow";
 import AddInput from 'components/pages/note/AddItem/AddItem'
+import Button from "components/common/Button/Button";
 import { useDispatch } from 'react-redux';
 import { updateTodoList } from 'app/reducers/notesReducer'
 import { useHistory } from "react-router-dom";
 
-function TodoList({note, button}) {
+function TodoList({note}) {
     const {todoList, largestIdTodo, title} = note;
     const [newTodos, editTodoList] = useState(todoList.map(a => Object.assign({}, a)));
     const [tempTodos, editTempTodoList] = useState([]);
@@ -17,17 +18,21 @@ function TodoList({note, button}) {
     const dispatch = useDispatch();
     let history = useHistory();
 
-    useEffect(() => {
+    const handleClick = (button) => {
         switch(button) {
             case 'reset':
+                // Если нажата reset, то клонируем текущее состоянии во временный массив, а текущее заменяем на исходное
                 editTempTodoList(newTodos.map(a => Object.assign({}, a)));
                 editTodoList(todoList.map(a => Object.assign({}, a)));
                 break;
             case 'redo':
+                // Если redo нажата раньше reset (временный массив пустой), то ничего не делаем
                 if (!tempTodos.length) break;
+                // Если нажата redo, то клонируем в массив с текущим состоянием временный массив
                 editTodoList(tempTodos.map(a => Object.assign({}, a))); 
                 break;
             case 'save':
+                // При сохранении dispatch-им текущее состояние в store И возвращемся на главную страницу 
                 dispatch(updateTodoList({
                     id: note.id,
                     largestIdTodo: currentLargestIdTodo,
@@ -35,8 +40,12 @@ function TodoList({note, button}) {
                 }))
                 history.push("/");
                 break;
+            case 'cancel':
+                // если нажата cancel, запускаем диалоговое окно для уточнения решения
+                toggleOpenModal(true);
+                break;
         }
-    }, [button])
+    }
 
     const addTodo = (todoTitle) => {
         newTodos.push({
@@ -50,14 +59,6 @@ function TodoList({note, button}) {
     const deleteTodo = (id) => {
         editIdTodo(id);
         toggleOpenModal(true);
-    }
-
-    const handleModalClick = (clickYes) => {
-        if (clickYes) {
-            editTodoList(newTodos.filter((todo) => todo.id !== idTodo))
-        }
-        toggleOpenModal(false);
-        editIdTodo('');
     }
 
     const toggleTodo = (id) => {
@@ -80,13 +81,32 @@ function TodoList({note, button}) {
         editTodoList(todos);
     }
 
+    const acceptModal = () => {
+        // диалоговое окно на текущей странице открывается только на удаление или отмену изменений.
+        // если idTodo не пустое, значит диалоговое окно вызвано для удаления
+        // иначе оно вызвано для отмены изменений
+        if (idTodo) {
+            editTodoList(newTodos.filter((todo) => todo.id !== idTodo))
+        } else {
+            history.push("/");
+        }
+        toggleOpenModal(false);
+        editIdTodo('');
+    }
+
     return <div className="todolist">
         <h2>{title}</h2>
-        <AddInput placeholder="Add new Todo" addNewItem={addTodo} />
+        <AddInput placeholder="Add new Todo" buttonValue="Add" addNewItem={addTodo} />
         {newTodos.map((todo) => 
             <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} editDesc={editDesc} />
         )} 
-        {openModal ?  <DialogWindow accept={() => handleModalClick(true)} reject={() => handleModalClick(false)} /> : ''}
+        <div className="todolist__buttons">
+            <Button onClick={() => handleClick('reset')} className="button button--blue" value="Reset"/>
+            <Button onClick={() => handleClick('redo')} className="button button--black" value="Redo Change"/>
+            <Button onClick={() => handleClick('save')} className="button" value="Save"/>
+            <Button onClick={() => handleClick('cancel')} className="button button--red" value="Cancel"/>
+        </div>
+        {openModal ?  <DialogWindow accept={() => acceptModal()} reject={() => toggleOpenModal(false)} /> : ''}
     </div>
 }
 
